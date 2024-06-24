c0945e18a853149edbaf1950b603a0f7d2ccc705
木村歩美 <kimuraayumi@kimuraayuminonotobukkukonpyuta.local>
2024-06-24 15:53:23 +0900

    APIの情報削除

--- src/pages/getApi.tsx
+++ src/pages/getApi.tsx
@@ -4,9 +4,10 @@ export const getApi = async () => {
     try{
         const response = await axios.get("https://opendata.resas-portal.go.jp/api/v1/prefectures/", {
             headers: {
-                "X-API-KEY": "scLC3NvRgfv9XvTuCQDhVaAWOhDCJLZNsrBjzgmL"
+                "X-API-KEY": process.env.NEXT_PUBLIC_RESAS_API_KEY,
             }
         });
+        console.log(response.data.result);
         return(response.data.result);
     }
     catch(error){
